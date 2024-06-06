7049408b879714a6f7581ce6c224a97a787e6d30
ajay-durgam <ajayrio665@gmail.com>
2024-06-06 11:06:51 +0530

    Employee is updated

--- src/com/edubridge/myproject1/model/Employee.java
+++ src/com/edubridge/myproject1/model/Employee.java
@@ -5,5 +5,23 @@ public class Employee {
 	private String employeeName;
 	private int employeeId;
 	private long employeeMobile;
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public long getEmployeeMobile() {
+		return employeeMobile;
+	}
+	public void setEmployeeMobile(long employeeMobile) {
+		this.employeeMobile = employeeMobile;
+	}
 
 }
